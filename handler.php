54d1c19bf380bad876b0ac278ecdfb218b36e3f8
shuang768 <113189677+shuang768@users.noreply.github.com>
2022-10-28 15:27:30 +0000

    almost done

--- handler.php
+++ handler.php
@@ -1,4 +1,4 @@
-<?php if ( isset($_POST['diameter']) && is_numeric($_POST['diameter']) ) : ?>
+<?php if ( isset($_POST['diameter']) && (is_numeric($_POST['diameter']) && $_POST['diameter']>=0) ): ?>
 
 <!DOCTYPE HTML>
 <html>
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" href="index.css">
     <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
@@ -33,13 +34,12 @@
         echo "<form method=\"post\" name=\"whoareyou\" action=\"handler.php\">\n";
          echo "<div class=\"mdl-textfield mdl-js-textfield\">\n";
           echo  "<input class=\"mdl-textfield__input\" type=\"text\" id=\"diameter\" name=\"diameter\">\n";
-           echo "<label class=\"mdl-textfield__label\" for=\"diameter\"> What is the diameter?</label>\n";
-         echo "</div>\n";
+          echo "<label class=\"mdl-textfield__label\" for=\"diameter\"> What is the diameter?</label>\n";
+          if (isset($_GET) && $_GET['error'] == 1) {
+            echo "<p class=\"variablecolour\">please enter a valid number</p>";
+          }
+          echo "</div>\n";
       ?>
-      <?php if (isset($_GET) && $_GET['error'] == 1) {
-      echo "<p class=\"variablecolour\">please enter a valid number</p>";
-      }
-      ?>  
       <?php
          echo "<div>\n";
             echo "<button class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\">
