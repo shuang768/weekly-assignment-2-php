1deb05e58a3393d77ac94a86ccaccdb595b3edbd
shuang768 <113189677+shuang768@users.noreply.github.com>
2022-10-28 14:38:21 +0000

    try font color

--- handler.php
+++ handler.php
@@ -1,51 +1,3 @@
-<?php if ( isset($_POST) && is_int($_POST['diameter']) && is_float($_POST['diameter']) ) : ?>
-
-
-<!DOCTYPE HTML>
-<html>
-  
-  <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="My first php page" />
-    <meta name="keywords" content="ics2o" />
-    <meta name="author" content="shuang xia" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
-    <link rel="manifest" href="./site.webmanifest">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>My First PHP Page</title>
-  </head>
-  
-  <body>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">area and circumference of a circle</span>
-        </div>
-      </header>
-  
-      <main class="mdl-layout__content" style="margin-left:75px;">
-        
-        <?php
-          $_POST['diameter1']=$_POST['diameter']/2
-        ?>
-        <?php
-          echo "<h3>the circumference of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']*2*pi()."<h3>"; 
-        ?>
-        <?php
-          echo "<h3>the area of a circle with a diameter of ".$_POST['diameter']." is ".$_POST['diameter1']**2*pi()."<h3>"; 
-        ?>
-      </main>
-
-    </div>
-  </body>
-</html>
-
 <?php if ( isset($_POST['diameter']) && is_numeric($_POST['diameter']) ) : ?>
 
 <!DOCTYPE HTML>
@@ -94,10 +46,10 @@
   </body>
 </html>
 
-<?php else : ?>
+<?php else : header("Location:http://ics2o.ca/~shuang/index.php?error=1"); ?>
 
 <?php
 // Send them back to index.php?error=1
-header("Location:http://ics2o.ca/~shuang/index.php?error=1");
+//header("Location:http://ics2o.ca/~shuang/index.php?error=1");
 ?>
 <?php endif; ?>
--- index.php
+++ index.php
@@ -18,10 +18,6 @@
   </head>
   
   <body>
-    <?php if (isset($_GET) && $_GET['error'] == 1) {
-      echo "please enter a valid number";
-    }
-    ?> 
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
       
       <header class="mdl-layout__header">
@@ -32,13 +28,19 @@
         
       <main class="mdl-layout__content" style="margin-left:75px;">
         <!-- Simple Textfield -->
+        
       <?php
         echo "<form method=\"post\" name=\"whoareyou\" action=\"handler.php\">\n";
          echo "<div class=\"mdl-textfield mdl-js-textfield\">\n";
           echo  "<input class=\"mdl-textfield__input\" type=\"text\" id=\"diameter\" name=\"diameter\">\n";
            echo "<label class=\"mdl-textfield__label\" for=\"diameter\"> What is the diameter?</label>\n";
          echo "</div>\n";
-          
+      ?>
+      <?php if (isset($_GET) && $_GET['error'] == 1) {
+      echo "<p class=\"variablecolour\">please enter a valid number</p>";
+      }
+      ?>  
+      <?php
          echo "<div>\n";
             echo "<button class=\"mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent\">
               Submit
